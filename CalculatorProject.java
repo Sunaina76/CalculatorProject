8e963718f1fc45b76c2b8ad965c3da629cdea9fc
akshayakinnera9 <akshayakinnera@gmail.com>
2026-08-25 11:37:02 +0530

    Calculator java program with square feature

--- CalculatorProject.java
+++ CalculatorProject.java
@@ -1,5 +1,16 @@
 class CalculatorProject
   public int add(int a, int b) {
+	  int c=a+b;
+	  return c;
+  }
+public int square(int x)
+{
+	int z=x*x;
+	return z;
+}
     public static void main (String args[]) {
+	    CalculatorProject cal = new Calculator();
+	    system.out.println("the sum of two numbers is "+(cal.add(2,3)));
+	    system.out.println("the square of the number is "+(cal.square(4));
     }
   }
